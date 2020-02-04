b3c4d1f463cc14871d429e5998605619c53fa8aa
Than1st <58428798+Than1st@users.noreply.github.com>
2020-02-04 17:08:36 +0700

    Edit Dikit

---
+++ DataCalas/app/src/main/java/com/example/datacalas/tampilcalas.java
@@ -0,0 +1,14 @@
+package com.example.datacalas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tampilcalas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilcalas);
+    }
+}
