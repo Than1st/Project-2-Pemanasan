2a73b18239d76e67d2fe0030f0d33267642a620c
Than1st <58428798+Than1st@users.noreply.github.com>
2020-02-10 13:15:38 +0700

    update

---
+++ calas.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 10, 2020 at 07:13 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `calas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+CREATE TABLE `data` (
+  `nim` char(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `posisi` varchar(100) NOT NULL,
+  `gajih` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- coba.sql
+++ coba.sql
@@ -1,52 +1,67 @@
--- MariaDB dump 10.17  Distrib 10.4.6-MariaDB, for Win64 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: coba
--- ------------------------------------------------------
--- Server version	10.4.6-MariaDB
+-- Host: 127.0.0.1
+-- Generation Time: Feb 10, 2020 at 07:03 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `coba`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `data`
 --
 
-DROP TABLE IF EXISTS `data`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `data` (
-  `nim` int(10) NOT NULL AUTO_INCREMENT,
+  `nim` int(10) NOT NULL,
   `nama` varchar(50) NOT NULL,
-  `jk` set('Laki-Laki','Perempuam') NOT NULL,
-  PRIMARY KEY (`nim`)
-) ENGINE=InnoDB AUTO_INCREMENT=1912500956 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `jk` enum('L','P') NOT NULL,
+  `alamat` text NOT NULL,
+  `peminatan` varchar(30) NOT NULL,
+  `agama` enum('islam','katolik','protestan','budha','hindu','konghucu') NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `data`
 --
 
-LOCK TABLES `data` WRITE;
-/*!40000 ALTER TABLE `data` DISABLE KEYS */;
-INSERT INTO `data` VALUES (1911500641,'Sulthan Laksono Ramadhan',''),(1912500939,'Ine Widyaningrum Mustama Putri',''),(1912500942,'Daffa Febri Nugroho',''),(1912500943,'Muhammad Ajie Panca Kurniawan',''),(1912500944,'Armand Pramudia',''),(1912500945,'Dioda Zenner ',''),(1912500946,'Benita Hasna Raissa',''),(1912500948,'Nisrina Megaputri Yoniton',''),(1912500949,'Ibnu Ramadhan Arbiansyah','');
-/*!40000 ALTER TABLE `data` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `data` (`nim`, `nama`, `jk`, `alamat`, `peminatan`, `agama`, `email`) VALUES
+(1912500942, 'Daffa Febri Nugroho', 'L', 'Meruya', 'Pemrograman', 'islam', '1912500942@student.budiluhur.ac.id'),
+(1912500943, 'Muhammad Ajie Panca Kurniawan', 'L', 'Jl. Makam Pari 2', 'Pemrograman', 'islam', '1912500943@student.budiluhur.ac.id'),
+(1912500944, 'Armand Pramudia', '', 'Jakarta', '', 'islam', '1911500518@student.budiluhur.ac.id'),
+(1912500945, 'Dioda Zenner ', '', '1', '', 'islam', ''),
+(1912500946, 'Benita Hasna Raissa', '', '22', '', 'islam', ''),
+(1912500948, 'Nisrina Megaputri Yoniton', '', '333', '', 'islam', ''),
+(1912500949, 'Ibnu Ramadhan Arbiansyah', '', '333', '', 'islam', '');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-02-04  8:26:21
